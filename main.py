c90d3077bc3b1a78e56f76cda279a5509a5be90f
sachin801 <sachinsahara2002@gmail.com>
2021-10-05 18:43:59 +0530

    adding initial converstion

--- main.py
+++ main.py
@@ -19,6 +19,29 @@ def speak(text):
 speak('on')
 print('listening...')
 
+def initial():
+    print('listening...')
+    speak('hi are you doing fine')
+    while True:
+        data = stream.read(8000)
+        if len(data) == 0:
+            print('returned')
+            break
+        if rec.AcceptWaveform(data):
+            text = rec.Result()
+            text = json.loads(text)
+
+            print(text['text'])
+            if text['text'] == 'yes' or text['text'] == 'yeah':
+                #print('yes')
+                speak('i hoped so this made my day')
+                return
+            elif text['text'] == 'no' or text['text'] == 'nah':
+                speak('what i can do to lighten your mood')
+                return
+            else:
+                speak('sorry i could not understand')
+
 while True:
     #print('listening...')
     data = stream.read(8000)
@@ -26,11 +49,14 @@ while True:
         break
     if rec.AcceptWaveform(data):
 
-        text=rec.Result()
-        text=json.loads(text)
-        if text['text']=='jarvis' or text['text']=='hi jarvis' or text['text']=='hello jarvis' :
+        text = rec.Result()
+        text = json.loads(text)
+        print(text['text'])
+        if text['text'] == 'jarvis' or text['text']=='hi jarvis' or text['text']=='hello jarvis' :
             print(text['text'])
             break
+
+initial()
 while True:
 
     data = stream.read(8000)
