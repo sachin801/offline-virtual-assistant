36ba5998aa96bb25410c797e084e0c26535e4628
sachin801 <sachinsahara2002@gmail.com>
2021-10-05 18:11:28 +0530

    adding text to speech pyttsx3

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from vosk import Model, KaldiRecognizer
 import os
 import pyaudio
 import json
+import pyttsx3
 
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
@@ -10,10 +11,16 @@ p = pyaudio.PyAudio()
 stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=8000)
 stream.start_stream()
 
+def speak(text):
+    engine=pyttsx3.init()
+    engine.say(text)
+    engine.runAndWait()
+
+speak('on')
 print('listening...')
 while True:
 
-    data = stream.read(4000)
+    data = stream.read(8000)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -21,4 +28,5 @@ while True:
         text = rec.Result()
         text = json.loads(text)
         print(text['text'])
+        speak(text['text'])
 
